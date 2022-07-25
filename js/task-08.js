a05001700242c0e349c07f27983d4d62d751ab74
Denis Orlyansky <000slim001@gmail.com>
2022-07-25 22:14:01 +0300

    8-th task complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Fill in all the fields!");
+  }
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    console.log(name, value);
+  });
+
+  event.currentTarget.reset();
+}
