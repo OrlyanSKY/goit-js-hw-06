831233053d7c6e8a5a1cd06792451ac51593b034
Denis Orlyansky <000slim001@gmail.com>
2022-07-26 00:20:04 +0300

    9-th task complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  bodyRef: document.body,
+  currentColorValue: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+};
+
+refs.changeColorBtn.addEventListener("click", onClickChangeColorBtn);
+
+function onClickChangeColorBtn() {
+  refs.bodyRef.style.backgroundColor = getRandomHexColor();
+  refs.currentColorValue.textContent = getRandomHexColor();
+}
