b65d7632fee8cc676bf3b7314f6839452c2d9fd2
Denis Orlyansky <000slim001@gmail.com>
2022-07-26 14:07:02 +0300

    bonus task complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  controls: document.querySelector("#controls"),
+  inputField: document.querySelector("input"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener("click", createBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let divSize = 30;
+
+  for (let i = 0; i < refs.inputField.value; i++) {
+    const innerDiv = document.createElement("div");
+
+    innerDiv.style.width = `${divSize}px`;
+    innerDiv.style.height = `${divSize}px`;
+    innerDiv.style.backgroundColor = getRandomHexColor();
+
+    divSize += 10;
+
+    boxes.append(innerDiv);
+  }
+}
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = "";
+}
